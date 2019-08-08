e6231be0b6e7867edf7c775612177f89573c3a51
juliadavies <42846955+juliadavies@users.noreply.github.com>
2019-08-08 13:45:05 -0500

    finished game

---
+++ GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { MAIN_MENU, LEVEL1, LEVEL2 };
+public delegate void OnStateChangeHandler();
+
+public class GameManager : MonoBehaviour
+{
+        private static GameManager instance = null;
+        public event OnStateChangeHandler OnStateChange;
+        public GameState gameState { get; private set; }
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (GameManager.instance == null)
+                {
+                    DontDestroyOnLoad(GameManager.instance);
+                    GameManager.instance = new GameManager();
+                }
+                return GameManager.instance;
+            }
+        }
+
+        public void SetGameState(GameState state)
+        {
+            this.gameState = state;
+            OnStateChange();
+        }
+
+        public void OnApplicationQuit()
+        {
+            GameManager.instance = null;
+        }
+}
---
+++ exitscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+        }
+    }
+}
